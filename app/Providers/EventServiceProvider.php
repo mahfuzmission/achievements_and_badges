507df082e78127d5a93f8499517231c155ae08a8
mahfuzmission <mahfuzmission.aiub@gmail.com>
2022-05-16 06:02:33 +0600

    relevent event & listener added

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace App\Providers;
 
+use App\Events\AchievementUnlocked;
+use App\Events\BadgeUnlocked;
 use App\Events\LessonWatched;
 use App\Events\CommentWritten;
+use App\Listeners\AchievementUnlockedListener;
+use App\Listeners\BadgeUnlockedListener;
+use App\Listeners\CommentWrittenListener;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -16,11 +21,17 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         CommentWritten::class => [
-            //
+            CommentWrittenListener::class
         ],
         LessonWatched::class => [
-            //
+            LessonWatched::class
         ],
+        AchievementUnlocked::class => [
+            AchievementUnlockedListener::class
+        ],
+        BadgeUnlocked::class => [
+            BadgeUnlockedListener::class
+        ]
     ];
 
     /**
