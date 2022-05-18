a9e95cfafe905271871ac531d2d72fe532898e27
mahfuzmission <mahfuzmission.aiub@gmail.com>
2022-05-18 07:00:45 +0600

    achivement api complete

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -2,19 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Achievement;
+use App\Models\Badge;
 use App\Models\User;
+use App\Models\UserAchievement;
+use App\Models\UserAchievementHistory;
 use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
     public function index(User $user)
     {
+        $unlocked_achievements = UserAchievementHistory::where('user_id', $user->id)->pluck('achievement_name', 'achievement_slug')->toArray();
+        $next_available_achievements = Achievement::whereNotIn('slug', array_keys($unlocked_achievements))->pluck('name');
+
+        $current_badge_id = $user->achievement->badge_id ?? 1;
+        $next_badge_id = $current_badge_id + 1;
+        $current_achievement = $user->achievement->total_earned_achievement ?? 0;
+
+        $badge = Badge::whereIn('id', [$current_badge_id, $next_badge_id])->select('badge_name','achievement_required')->get();
+
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => array_values($unlocked_achievements),
+            'next_available_achievements' => $next_available_achievements,
+            'current_badge' => $badge[0]->badge_name,
+            'next_badge' => $badge[1]->badge_name ?? '',
+            'remaing_to_unlock_next_badge' => isset($badge[1]) ? ( $badge[1]->achievement_required - $current_achievement ) : 0
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\AchievementsController;
 
 Route::get('/users/{user}/achievements', [AchievementsController::class, 'index']);
 
-
 Route::get('/', function (){
 
     $user = \App\Models\User::find(1);
@@ -15,29 +14,32 @@ Route::get('/', function (){
         $user = \App\Models\User::factory()->create();
     }
 
-    $data['lesson'] = \App\Models\Lesson::factory()->count(5)->create();
+//    $data['lesson'] = \App\Models\Lesson::factory()->count(10)->create();
+//
+//    foreach ($data['lesson'] as $lesson)
+//    {
+//        \Illuminate\Support\Facades\DB::table('lesson_user')->insert([
+//            'watched' => 1,
+//            'lesson_id' => $lesson->id,
+//            'user_id' => $user->id,
+//        ]);
+//
+//        event(new \App\Events\LessonWatched($lesson, $user));
+//    }
 
-    foreach ($data['lesson'] as $lesson)
+//        $data['comment'] = \App\Models\Comment::factory()->count(20)->create();
+
+    for($i = 0; $i <= 20; $i++)
     {
-        \Illuminate\Support\Facades\DB::table('lesson_user')->insert([
-            'watched' => 1,
-            'lesson_id' => $lesson->id,
-            'user_id' => $user->id,
+        $comment = \App\Models\Comment::create([
+            'body' => "This is my $i comment",
+            'user_id'=> $user->id
         ]);
 
-        event(new \App\Events\LessonWatched($lesson, $user));
+        event( new \App\Events\CommentWritten($comment));
+        $data['comment'][] = $comment;
     }
 
-    //    $data['comment'] = \App\Models\Comment::factory()->count(20)->create();
-//    $comment = \App\Models\Comment::create([
-//        'body' => "This is my first comment",
-//        'user_id'=> $user->id
-////        'user_id'=> 1
-//    ]);
-//
-//    event( new \App\Events\CommentWritten($comment));
-//
-//    $data['comment'] = $comment;
 
     return response()->json(["data" => $data]);
 });
